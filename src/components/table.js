8ab0e35ccda497494d3200cfc4054309e09fbcc7
natalia <skaweb@yandex.ru>
2026-05-01 02:54:14 +0300

    Обработчики root.container

--- src/components/table.js
+++ src/components/table.js
@@ -12,8 +12,27 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container);
+    });
+
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);
+    })
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener("change", () => onAction());
+
+    root.container.addEventListener("reset", (e) => setTimeout(onAction));
+
+    root.container.addEventListener("submit", (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
+
+
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
