f89d2575cacc29789e417b4b65534dcc1b824963
Kirill <skaweb@yandex.ru>
2026-05-01 17:45:11 +0300

    Доделать пагинацию

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -15,6 +15,21 @@ export const initPagination = (
     let page = state.page; // страница переменной, потому что она может меняться при обработке действий позже
 
     // @todo: #2.6 — обработать действия
+    if (action)
+      switch (action.name) {
+        case "prev":
+          page = Math.max(1, page - 1);
+          break; // переход на предыдущую страницу
+        case "next":
+          page = Math.min(pageCount, page + 1);
+          break; // переход на следующую страницу
+        case "first":
+          page = 1;
+          break; // переход на первую страницу
+        case "last":
+          page = pageCount;
+          break; // переход на последнюю страницу
+      }
 
     // @todo: #2.4 — получить список видимых страниц и вывести их
     const visiblePages = getPages(page, pageCount, 5); // Получим массив страниц, которые нужно показать, выводим только 5 страниц
@@ -27,6 +42,9 @@ export const initPagination = (
     );
 
     // @todo: #2.5 — обновить статус пагинации
+    fromRow.textContent = (page - 1) * rowsPerPage + 1; // С какой строки выводим
+    toRow.textContent = Math.min(page * rowsPerPage, data.length); // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+    totalRows.textContent = data.length; // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
 
     // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
     const skip = (page - 1) * rowsPerPage; // сколько строк нужно пропустить
