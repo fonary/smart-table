334d6094998a9aa971d6448c7043d7cca0bc26ea
Kirill <skaweb@yandex.ru>
2026-05-04 00:25:45 +0300

    Searching

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,16 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        ["skipEmptyTargetValues"],
+        [rules.searchMultipleFields(searchField, ["date", "customer", "seller"], false)]
+    );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        if (action && action.type === "clear") {
+            return data;
+        }
+        return data.filter(row => compare(row, state));
     }
 }
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { initTable } from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 // @todo: подключение
 
 // Исходные данные используемые в render()
@@ -40,6 +41,7 @@ function render(action) {
   let state = collectState(); // состояние полей из таблицы
   let result = [...data]; // копируем для последующего изменения
   // @todo: использование
+  result = applySearching(result, state, action);
   result = applyFiltering(result, state, action);
   result = applySorting(result, state, action);
   result = applyPagination(result, state, action);
@@ -51,7 +53,7 @@ const sampleTable = initTable(
   {
     tableTemplate: "table",
     rowTemplate: "row",
-    before: ["header", "filter"],
+    before: ["search", "header", "filter"],
     after: ["pagination"],
   },
   render,
@@ -77,6 +79,8 @@ const applySorting = initSorting([
   sampleTable.header.elements.sortByTotal,
 ]);
 
+
+const applySearching = initSearching("search");
 
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
   // передаём элементы фильтра
